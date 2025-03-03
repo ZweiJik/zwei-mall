84185965b0c31875365c187a46b72c1e47c188a0
ZweiJik <wxdyiziwei@163.com>
2025-03-03 13:52:17 +0800

    init project

---
+++ mall-producet/src/test/java/com/zwei/mall/mallproducet/MallProducetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwei.mall.mallproducet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProducetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
